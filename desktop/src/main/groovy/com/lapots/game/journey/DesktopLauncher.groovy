e54bba2b6fb0bd0424852d20ce975714c5b462dc
Lapots <sebastrident@gmail.com>
2016-10-05 17:33:50 +0300

    Prototype for resource injection annotation

--- desktop/src/main/groovy/com/lapots/game/journey/DesktopLauncher.groovy
+++ desktop/src/main/groovy/com/lapots/game/journey/DesktopLauncher.groovy
@@ -2,14 +2,36 @@ package com.lapots.game.journey;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.lapots.game.journey.ApplicationMain;
+import groovy.json.JsonSlurper;
 
 public class DesktopLauncher {
 
+    /**
+     * Equals to
+     *      // check that method exist -> then continue
+     *      private static $classpathFileResource(String resource) {
+     *          def classLoader = Thread.currentThread().getContextClassLoader()
+     *          return new File(classLoader.getResource(resource).getFile())
+     *      }
+     *      // check that method exists -> then invoke
+     *      private static $jsonFileResource(String resource) {
+     *          return new groovy.json.JsonSlurper().parse($classpathFileResource(resource))
+     *      }
+     *
+     *      def config = $jsonFileResource("application.config")
+     */
+    @JsonResource("application.config")
+    def config
+
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1000;
-        config.height = 800;
+
+        def classLoader = Thread.currentThread().getContextClassLoader()
+        def jsonConfig = new JsonSlurper().parse(new File(classLoader.getResource("application.config").getFile()))
+
+        config.width = jsonConfig.window.width
+        config.height = jsonConfig.window.height
+
 
         new LwjglApplication(new ApplicationMain(), config);
     }
